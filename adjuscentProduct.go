caf203c16596f4081cc61bcd4af4faaa008f5a54
Julsan Magaju - 280983 <julsan.magaju@centene.com>
2022-03-19 11:24:50 -0500

    func

--- adjuscentProduct.go
+++ adjuscentProduct.go
@@ -1,3 +1,13 @@
 package Golang_Practice
 
+func solution(inputArray []int) int {
+	max := inputArray[0] * inputArray[1]
 
+	for i := 0; i < len(inputArray)-1; i++ {
+		if max < inputArray[i]*inputArray[i+1] {
+			max = inputArray[i] * inputArray[i+1]
+		}
+	}
+
+	return max
+}
